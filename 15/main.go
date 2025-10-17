972a2836ed2af8ae5f818c77e158f325c9d9fdf8
misshanya <mishanyadev@gmail.com>
2025-10-17 20:52:33 +0300

    feat(minishell): basic functional

---
+++ 15/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Println()
+				break
+			}
+
+			fmt.Println("failed to read line:", err)
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+
+		parts := strings.Fields(input)
+
+		if len(parts) < 1 {
+			continue
+		}
+
+		switch parts[0] {
+
+		case "cd":
+			var err error
+			if len(parts) > 1 {
+				err = os.Chdir(parts[1])
+			} else {
+				var homedir string
+				homedir, err = os.UserHomeDir()
+				if err != nil {
+					fmt.Println("failed to get user home dir")
+					continue
+				}
+				err = os.Chdir(homedir)
+			}
+			if err != nil {
+				fmt.Println("failed to execute cd:", err)
+				continue
+			}
+
+		case "pwd":
+			workDir, err := os.Getwd()
+			if err != nil {
+				fmt.Println("failed to get current workdir:", err)
+				continue
+			}
+			fmt.Println(workDir)
+
+		case "echo":
+			if len(parts) > 1 {
+				fmt.Println(strings.Join(parts[1:], " "))
+			}
+
+		case "exit":
+			os.Exit(0)
+
+		default:
+			fmt.Println("Unknown command")
+		}
+	}
+}
