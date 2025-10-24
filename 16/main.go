6bc6d817475968d0b53c6393a38a60f4838bd224
misshanya <mishanyadev@gmail.com>
2025-10-24 19:29:30 +0300

    feat(miniget): support <link>, <script> and <img> links

--- 16/main.go
+++ 16/main.go
@@ -20,14 +20,23 @@ func findLinks(body []byte) ([]string, error) {
 
 	var links []string
 
+	findAndAppendAttr := func(attrs []html.Attribute, key string) {
+		for _, a := range attrs {
+			if a.Key == key {
+				links = append(links, a.Val)
+				break
+			}
+		}
+	}
+
 	var f func(*html.Node)
 	f = func(node *html.Node) {
-		if node.Type == html.ElementNode && node.Data == "a" {
-			for _, a := range node.Attr {
-				if a.Key == "href" {
-					links = append(links, a.Val)
-					break
-				}
+		if node.Type == html.ElementNode {
+			switch node.Data {
+			case "a", "link":
+				findAndAppendAttr(node.Attr, "href")
+			case "script", "img":
+				findAndAppendAttr(node.Attr, "src")
 			}
 		}
 		for c := node.FirstChild; c != nil; c = c.NextSibling {
