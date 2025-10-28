4121167d9a75667d2fdc97bc7f4386444c90cdaf
misshanya <mishanyadev@gmail.com>
2025-10-28 12:00:31 +0300

    fix(minicalendar): make result slice with needed capacity instead of len for GetAll

--- 18/internal/repository/event/get_all.go
+++ 18/internal/repository/event/get_all.go
@@ -7,7 +7,7 @@ func (r *repo) GetAll() []*models.Event {
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
-	result := make([]*models.Event, len(r.storage))
+	result := make([]*models.Event, 0, len(r.storage))
 	for id, event := range r.storage {
 		e := eventToModel(event)
 		e.ID = id
