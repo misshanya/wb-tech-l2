89c8ddfc51de1acfb64ae34bdb3f5ad0b4067b2d
misshanya <mishanyadev@gmail.com>
2025-10-28 12:29:31 +0300

    chore(minicalendar): log successful event creation

--- 18/internal/service/event/create.go
+++ 18/internal/service/event/create.go
@@ -4,5 +4,7 @@ import "github.com/misshanya/wb-tech-l2/18/internal/models"
 
 // Create creates new event
 func (s *service) Create(e *models.Event) *models.Event {
-	return s.repo.Create(e)
+	finalEvent := s.repo.Create(e)
+	s.l.Info("created event", "event", e)
+	return finalEvent
 }
